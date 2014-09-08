431d77e80884885948ca5ff401eb64b3f60d07a4
szuletett <szuletett@gmail.com>
2014-09-08 08:31:30 -0500

    list view with columns. choose due from select tag. looks more organized

--- Gemfile
+++ Gemfile
@@ -13,7 +13,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 
 # use bootstrap
-gem 'bootstrap-sass', '2.3.2.0'
+gem 'bootstrap-sass', '~> 3.1.1'
 
 # whenever for scheduling
 gem 'whenever', :require => false
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -19,7 +20,7 @@ module Scrapout
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+#    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
 
     config.time_zone = 'Lima'
     
